bb21ba350e02ea3d5247fabb1aee59a578eb4d15
21MIC0181 <155751253+21MIC0181@users.noreply.github.com>
2024-04-22 13:50:47 +0530

    Add files via upload

---
+++ jenk.java
@@ -0,0 +1,8 @@
+public class jenk
+{
+    public static void main(String[] args) {
+        System.out.println("JANANI");
+        System.out.println("21MIC0181");
+        System.out.println("I am from Vit");
+    }
+}
